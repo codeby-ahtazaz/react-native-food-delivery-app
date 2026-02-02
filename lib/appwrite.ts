3733cc257d75eba3c9804416acfaacb6dea0a1cb
Ahtazaz <>
2026-02-02 16:08:57 +0500

    sign in and sign up UI implemented

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform: "com.easy.rnfoodapp",
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "6979e88000237d72e7d2",
+    userCollectionId: "user"
 }
